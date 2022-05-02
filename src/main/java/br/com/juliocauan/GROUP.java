95dea55213138faf4254fafbb65cbd5387a3d7c3
Julio Cauan <juliocauan@gmail.com>
2022-05-02 13:22:01 -0300

    implemented parseAttribute

---
+++ src/main/java/br/com/juliocauan/GROUP.java
@@ -0,0 +1,14 @@
+package br.com.juliocauan;
+
+public enum GROUP {
+    TITLE(2),
+    YEAR(4),
+    IMAGE_URL(5),
+    IMDB_RATING(7);
+
+    public int pos;
+
+    private GROUP(int pos) {
+        this.pos = pos;
+    }
+}
--- src/main/java/br/com/juliocauan/imdb_api_consumer.java
+++ src/main/java/br/com/juliocauan/imdb_api_consumer.java
@@ -8,6 +8,8 @@ import java.net.http.HttpClient.Version;
 import java.net.http.HttpResponse.BodyHandlers;
 import java.time.Duration;
 import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 public class imdb_api_consumer{
     public static void main(String[] args) throws Exception {
@@ -18,11 +20,26 @@ public class imdb_api_consumer{
 		HttpClient client = HttpClient.newBuilder().version(Version.HTTP_2).build();
 		HttpRequest request = HttpRequest.newBuilder().uri(top250MoviesURI).timeout(Duration.ofSeconds(30L)).header("Content-Type", "application/json").GET().build();
 		HttpResponse<String> response = client.send(request, BodyHandlers.ofString());
-		System.out.println(response.statusCode());
+		System.out.println("Status Code: " + response.statusCode());
 
 		String json = response.body();
         String[] movies = parseJsonMovies(json);
-        
+        List<String> titles = parseAttribute(movies, GROUP.TITLE);
+        List<String> urlImages = parseAttribute(movies, GROUP.IMAGE_URL);
+        List<String> years = parseAttribute(movies, GROUP.YEAR);
+        List<String> notas = parseAttribute(movies, GROUP.IMDB_RATING);
+        notas.forEach(System.out::println);
+    }
+
+    private static List<String> parseAttribute(String[] movies, GROUP group) {
+        return Stream.of(movies)
+            //Splits array by group
+            .map(movie -> movie.split("\",\"")[group.pos])
+            //Removes group name
+            .map(movie -> movie.split("\":\"")[1])
+            //Removes any remnant -> " <-
+            .map(movie -> movie.replaceAll("\"", ""))
+            .collect(Collectors.toList());
     }
 
     private static String[] parseJsonMovies(String json) {
