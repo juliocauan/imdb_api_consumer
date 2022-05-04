a0c0335942045bad73d06f5cbf8a0754d8c69ea6
Julio Cauan <juliocauan@gmail.com>
2022-05-03 21:08:30 -0300

    fixed .gitignore

--- src/main/java/br/com/juliocauan/imdb_api_consumer.java
+++ src/main/java/br/com/juliocauan/imdb_api_consumer.java
@@ -7,7 +7,7 @@ public class imdb_api_consumer{
     public static void main(String[] args) throws Exception {
 
         String apiKey = "k_jngird0l";
-        String htmlName = "Top250Movies";
+        String htmlName = "Top250Movies.html";
         
 		String json = new ImdbApiClient(apiKey).getBody();
         
