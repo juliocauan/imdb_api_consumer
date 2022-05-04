489ecb6269d30ddd9a741a37e532378b9708f4a7
Julio Cauan <juliocauan@gmail.com>
2022-05-04 15:58:47 -0300

    refactoring folders

--- src/main/java/br/com/juliocauan/imdb_api_consumer.java
+++ src/main/java/br/com/juliocauan/imdb_api_consumer.java
@@ -3,6 +3,10 @@ package br.com.juliocauan;
 import java.io.PrintWriter;
 import java.util.List;
 
+import br.com.juliocauan.model.api.apiClient.ImdbApiClient;
+import br.com.juliocauan.model.api.jsonParser.ImdbMovieJsonParser;
+import br.com.juliocauan.model.entity.Movie;
+
 public class imdb_api_consumer{
     public static void main(String[] args) throws Exception {
 
