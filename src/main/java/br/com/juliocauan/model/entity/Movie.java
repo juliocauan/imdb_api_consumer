16babf14c0bd895c92cde435791a0a011fb2f71a
Julio Cauan <juliocauan@gmail.com>
2022-05-04 16:48:36 -0300

    Encapsulated Movie Object

--- src/main/java/br/com/juliocauan/model/entity/Movie.java
+++ src/main/java/br/com/juliocauan/model/entity/Movie.java
@@ -1,38 +1,11 @@
 package br.com.juliocauan.model.entity;
 
-public class Movie {
+import br.com.juliocauan.model.entity.interfaces.Content;
 
-    private String title;
-    private String imageUrl;
-    private Float rating;
-    private Short year;
+public class Movie extends Content{
 
     public Movie(String title, String year, String rating, String imageUrl){
-        this.title = title;
-        this.year = Short.valueOf(year);
-        this.rating = Float.valueOf(rating);
-        this.imageUrl = imageUrl;
+        super(title, year, rating, imageUrl);
     }
-    public Short getYear() {
-        return year;
-    }
-    public Float getRating() {
-        return rating;
-    }
-    public String getImageUrl() {
-        return imageUrl;
-    }
-    public void setImageUrl(String imageUrl) {
-        this.imageUrl = imageUrl;
-    }
-    public String getTitle() {
-        return title;
-    }
-
-    @Override
-    public String toString() {
-        return String.format("|Title: %30s|\n|IMDb Rating: %24s|\n|Year: %31s|\n|Image Url: %s\n",
-        getTitle(), getRating(), getYear(), getImageUrl());
-    }
-
+    
 }
---
+++ src/main/java/br/com/juliocauan/model/entity/interfaces/Content.java
@@ -0,0 +1,39 @@
+package br.com.juliocauan.model.entity.interfaces;
+
+public abstract class Content {
+
+    private String title;
+    private String imageUrl;
+    private Float rating;
+    private Short year;
+
+    public Content(String title, String year, String rating, String imageUrl){
+        this.title = title;
+        this.year = Short.valueOf(year);
+        this.rating = Float.valueOf(rating);
+        this.imageUrl = imageUrl;
+    }
+    
+    public final Short getYear() {
+        return year;
+    }
+    public final Float getRating() {
+        return rating;
+    }
+    public final String getImageUrl() {
+        return imageUrl;
+    }
+    public final void setImageUrl(String imageUrl) {
+        this.imageUrl = imageUrl;
+    }
+    public final String getTitle() {
+        return title;
+    }
+
+    @Override
+    public String toString() {
+        return String.format("|Title: %30s|\n|Rating: %29s|\n|Year: %31s|\n|Image Url: %s\n",
+        getTitle(), getRating(), getYear(), getImageUrl());
+    }
+
+}
